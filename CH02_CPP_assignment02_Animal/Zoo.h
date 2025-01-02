aaf8b6366f7ac8ed617fb8de70b0d18f6a558f49
Sang-Hyun-Kim <960322ksh@hanmail.net>
2025-01-02 19:30:57 +0900

    CH02_Assignment3 구현

--- CH02_CPP_assignment02_Animal/Zoo.h
+++ CH02_CPP_assignment02_Animal/Zoo.h
@@ -4,6 +4,9 @@ class Zoo
 private:
 	int idx = 0;
 	class Animal* animals[10] = {nullptr,};
+	// 배열 동적할당으로 추가하는 기능 보다 최대 크기 보다 더 입력하게 되면 
+	// 추가를 거부하면서 동시에 외부 코드에서 생성한 heap 영역에 대한 해제 처리를 하는 코드로
+	// 구현함
 public:
 	Zoo() {};
 	~Zoo();
